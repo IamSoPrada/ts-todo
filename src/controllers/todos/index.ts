ff8cb82735b47afb404b3ebf3fa46dd8625c2430
IamSoPrada <9999goldcoin@gmail.com>
2020-10-20 12:27:40 +0300

    Added CRUD methods

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -29,9 +29,49 @@ const addTodo = async (req: Request, res: Response): Promise<void> => {
 
         res
             .status(201)
-            .json({ message: "Todo added", todo: newTodo, todos: allTodos })
+            .json({ message: "TODO ADDED", todo: newTodo, todos: allTodos })
     } catch (error) {
         throw error
     }
-
 }
+
+
+const updateTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { id },
+            body,
+        } = req
+        const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+            { _id: id },
+            body
+        )
+        const allTodos: ITodo[] = await Todo.find()
+        res.status(200).json({
+            message: "TODO UPDATED",
+            todo: updateTodo,
+            todos: allTodos
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+            req.params.id
+        )
+        const allTodos: ITodo[] = await Todo.find()
+        res.status(200).json({
+            message: "TODO DELETED",
+            todo: deletedTodo,
+            todos: allTodos
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+export { getTodos, addTodo, updateTodo, deleteTodo }
